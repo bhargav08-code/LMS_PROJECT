25c903dea9c4bec3a9624a232c20dce1c3036270
bhargav08-code <bhargavgarge1828@gmail.com>
2024-03-16 03:00:34 +0530

    updated

--- src/components/ContractorTransaction.js
+++ src/components/ContractorTransaction.js
@@ -10,6 +10,7 @@ import {
   Thead,
   Tbody,
   Tr,
+  HStack,
   Th,
   Td,
   Button,
@@ -18,15 +19,21 @@ import {
 import { useData } from "../Context";
 import { useEffect, useState } from "react";
 import axios from "axios";
+
 const ContractorTransaction = () => {
   const { constructionData } = useData();
+  const [amount, setAmount] = useState("");
+  const [cheqNo, setCheqNo] = useState("");
+  const [remarks, setRemarks] = useState("");
   const [lessPercent, setLessPercent] = useState(0);
-  const [amountPaid, setAmountPaid] = useState(0);
   const [transactionDate, setTransactionDate] = useState("");
+  const [totalPaid, setTotalPaid] = useState(0);
+  const [totalBalance, setTotalBalance] = useState(0);
+  const [totalPayable, setTotalPayable] = useState(0);
   const [fetchData, setFetchData] = useState([""]);
-  const toast = useToast();
+  const toast = useToast(); // Define useToast hook
   const loadData = async () => {
-    let query = "SELECT * FROM `contractortransaction`;";
+    let query = "SELECT * FROM `contractorTransaction`;";
 
     const url = "https://lkgexcel.com/backend/getQuery.php";
     let fData = new FormData();
@@ -46,30 +53,40 @@ const ContractorTransaction = () => {
       console.log("Please Select Proper Input");
     }
   };
+  // Calculate total payable only once when the component mounts
+  useEffect(() => {
+    const amtPayable =
+      constructionData.amount - (constructionData.amount * lessPercent) / 100;
+    const updatedTotalBalance = isNaN(amtPayable) ? 0 : amtPayable;
+    setTotalPayable(updatedTotalBalance);
+    setTotalBalance(updatedTotalBalance); // Initialize total balance with total payable
+  }, [constructionData, lessPercent]);
 
-  const amtPayable =
-    constructionData.amount - (constructionData.amount * lessPercent) / 100;
-  const totalAmountPayable = isNaN(amtPayable) ? 0 : amtPayable;
-  const amountBalance = totalAmountPayable - (amountPaid || 0);
-
-  const addTransaction = async () => {
+  const handleSubmit = async () => {
     try {
+      const amountPaid = parseFloat(amount);
+      const updatedTotalPaid = totalPaid + amountPaid;
+
+      const updatedTotalBalance = totalBalance - amountPaid; // Update total balance
+
       const data = {
         contractor: constructionData.contractor,
         projectName: constructionData.projectName,
         blockName: constructionData.blockName,
         plotNo: constructionData.plotNo,
-        amount: constructionData.amount,
         lessPercent: lessPercent,
-        amtPayable: totalAmountPayable,
-        amountPaid: amountPaid,
-        amountBalance: amountBalance,
         date: transactionDate,
+        cheqNo: cheqNo,
+        remarks: remarks,
+        totalPaid: updatedTotalPaid,
+        totalBalance: updatedTotalBalance, // Assign the updated total balance
+        totalPayable: totalPayable, // Assign the total payable without recalculating
+        amount: amount,
       };
+      console.log(data);
 
       const url = "https://lkgexcel.com/backend/setQuery.php";
-      const query = `INSERT INTO contractortransaction (contractor, projectName, blockName, plotNo, amount, less, totalPayable, totalPaid, totalBal, date) VALUES ('${data.contractor}', '${data.projectName}', '${data.blockName}', '${data.plotNo}', '${data.amount}', '${data.lessPercent}', '${data.amtPayable}', '${data.amountPaid}', '${data.amountBalance}', '${data.date}')`;
-
+      const query = `INSERT INTO contractorTransaction (contractor, projectName, blockName, plotNo, amount, lessPercent, cheqNo, remarks, transactionDate, totalPaid, totalBalance, totalPayable) VALUES ('${data.contractor}', '${data.projectName}', '${data.blockName}', '${data.plotNo}', '${data.amount}', '${data.lessPercent}', '${data.cheqNo}', '${data.remarks}', '${data.date}', '${data.totalPaid}', '${data.totalBalance}', '${data.totalPayable}')`;
       const formData = new FormData();
       formData.append("query", query);
 
@@ -87,10 +104,18 @@ const ContractorTransaction = () => {
           position: "top",
           isClosable: true,
         });
+        setTotalPaid(updatedTotalPaid); // Update total paid state
+        setTotalBalance(updatedTotalBalance); // Update total balance state
         await loadData();
       } else {
         console.error("Failed to save contractor transaction:", response.data);
       }
+
+      // Reset form fields after successful submission
+      setAmount("");
+      setCheqNo("");
+      setRemarks("");
+      setTransactionDate("");
     } catch (error) {
       console.error("Error saving contractor transaction:", error.message);
     }
@@ -98,10 +123,9 @@ const ContractorTransaction = () => {
   useEffect(() => {
     loadData();
   }, []);
-
   return (
-    <Box display={"flex"} height={"80vh"} maxW={"100vw"}>
-      <Box flex={"15%"} borderRight={"1px solid grey"}>
+    <Box display={"flex"} height={"100vh"} maxW={"100vw"}>
+      <Box flex={"19%"} borderRight={"1px solid grey"}>
         <VStack alignItems={"flex-start"} gap={8}>
           <Text fontSize={"18px"} fontWeight={"semibold"}>
             Contractor :- {constructionData.contractor}
@@ -116,7 +140,7 @@ const ContractorTransaction = () => {
             Plot No :- {constructionData.plotNo}
           </Text>
           <Text fontSize={"18px"} fontWeight={"semibold"}>
-            Amount :- {constructionData.amount}
+            Construction Amount :- {constructionData.amount}
           </Text>
           <FormControl>
             <Flex align="center" justifyContent={"space-between"}>
@@ -135,61 +159,93 @@ const ContractorTransaction = () => {
             </Flex>
           </FormControl>
           <Text fontSize={"18px"} fontWeight={"semibold"}>
-            Amount Payable :- {totalAmountPayable}
-          </Text>{" "}
-          <FormControl>
-            <Flex align="center" justifyContent={"space-between"}>
-              <FormLabel fontSize={"18px"} fontWeight={"semibold"}>
-                Amount Paid :-
-              </FormLabel>
-              <Input
-                type="number"
-                placeholder="Enter Value"
-                w={"40%"}
-                position={"relative"}
-                right={"40px"}
-                value={amountPaid}
-                onChange={(e) => setAmountPaid(parseFloat(e.target.value))}
-              />
-            </Flex>
-          </FormControl>
-          <Text fontSize={"18px"} fontWeight={"semibold"}>
-            Amount Balance :- {amountBalance}
+            Total Payable :- {totalPayable}
           </Text>
-          <FormControl>
-            <Flex align="center" justifyContent={"space-between"}>
-              <FormLabel fontSize={"18px"} fontWeight={"semibold"}>
-                Date :-
-              </FormLabel>
+          <Text fontSize={"18px"} fontWeight={"semibold"}>
+            Total Paid :- {totalPaid}
+          </Text>{" "}
+          <Text fontSize={"18px"} fontWeight={"semibold"}>
+            Total Bal :- {totalBalance}
+          </Text>{" "}
+        </VStack>
+      </Box>
+      <Box flex={"89%"} maxW={"89%"}>
+        <Text marginLeft={"10px"}>Contractor Transaction</Text>
+        <Box
+          display="flex"
+          alignItems={"center"}
+          justifyContent={"center"}
+          mt={"15px"}
+        >
+          <HStack alignItems={"flex-start"}>
+            <FormControl>
+              <FormLabel>Date :-</FormLabel>
               <Input
                 type="date"
-                w={"60%"}
-                position={"relative"}
-                right={"40px"}
                 value={transactionDate}
                 onChange={(e) => setTransactionDate(e.target.value)}
               />
-            </Flex>
-          </FormControl>
-          <Button onClick={addTransaction}>Save</Button>
-        </VStack>
-      </Box>
-      <Box flex={"85%"} maxW={"80%"}>
-        <Text marginLeft={"10px"}>Contractor Transaction</Text>
-        <Table variant="simple" marginTop={"20px"}>
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Amount</FormLabel>
+              <Input
+                id="amount"
+                type="number"
+                value={amount} // Value from state
+                onChange={(e) => setAmount(e.target.value)} // Update state onChange
+              />
+            </FormControl>
+
+            {/* Input field for Cheq/Ref No */}
+            <FormControl>
+              <FormLabel>Chq/Ref No</FormLabel>
+              <Input
+                id="cheqNo"
+                type="text"
+                value={cheqNo} // Value from state
+                onChange={(e) => setCheqNo(e.target.value)} // Update state onChange
+              />
+            </FormControl>
+
+            {/* Input field for Remarks */}
+            <FormControl>
+              <FormLabel>Remarks</FormLabel>
+              <Input
+                id="remarks"
+                type="text"
+                value={remarks} // Value from state
+                onChange={(e) => setRemarks(e.target.value)} // Update state onChange
+              />
+            </FormControl>
+
+            <Button
+              colorScheme="telegram"
+              alignSelf={"flex-end"}
+              size={"md"}
+              w={"60%"}
+              mt={"30px"}
+              onClick={handleSubmit}
+            >
+              Submit
+            </Button>
+          </HStack>
+        </Box>
+
+        <Table variant="simple" marginTop={"20px"} size="sm">
           <Thead>
             <Tr bg={"#121212"} color={"whitesmoke"}>
               <Th color={"white"} border="1px solid black">
                 Contractor
               </Th>
               <Th color={"white"} border="1px solid black">
-                Project Name
+                Project
               </Th>
               <Th color={"white"} border="1px solid black">
-                Block Name
+                Block
               </Th>
               <Th color={"white"} border="1px solid black">
-                Plot No
+                Plot
               </Th>
               <Th color={"white"} border="1px solid black">
                 Amount
@@ -198,16 +254,25 @@ const ContractorTransaction = () => {
                 Less(%)
               </Th>
               <Th color={"white"} border="1px solid black">
-                Amt Payable
+                Cheque
               </Th>{" "}
               <Th color={"white"} border="1px solid black">
-                Amt Paid
-              </Th>
-              <Th color={"white"} border="1px solid black">
-                Amt Bal
+                Remarks
               </Th>
               <Th color={"white"} border="1px solid black">
                 Date
+              </Th>
+              <Th color={"white"} border="1px solid black">
+                Paid
+              </Th>{" "}
+              <Th color={"white"} border="1px solid black">
+                Balance
+              </Th>{" "}
+              <Th color={"white"} border="1px solid black">
+                Payable
+              </Th>
+              <Th color={"white"} border="1px solid black">
+                Action
               </Th>
             </Tr>
           </Thead>
@@ -225,11 +290,21 @@ const ContractorTransaction = () => {
                     <Td border="1px solid black">{data.blockName}</Td>
                     <Td border="1px solid black">{data.plotNo}</Td>
                     <Td border="1px solid black">{data.amount}</Td>
-                    <Td border="1px solid black">{data.less}</Td>
-                    <Td border="1px solid black">{data.totalPayable}</Td>
+                    <Td border="1px solid black">{data.lessPercent}</Td>
+                    <Td border="1px solid black">{data.cheqNo}</Td>
+                    <Td border="1px solid black">{data.remarks}</Td>
+                    <Td border="1px solid black">{data.transactionDate}</Td>
                     <Td border="1px solid black">{data.totalPaid}</Td>
-                    <Td border="1px solid black">{data.totalBal}</Td>
-                    <Td border="1px solid black">{data.date}</Td>
+                    <Td border="1px solid black">{data.totalBalance}</Td>
+                    <Td border="1px solid black">{data.totalPayable}</Td>
+                    <Td display={"flex"} border="1px solid black" gap={"5px"}>
+                      <Button colorScheme="green" size="sm">
+                        Edit
+                      </Button>
+                      <Button colorScheme="red" size="sm">
+                        Delete
+                      </Button>
+                    </Td>
                   </Tr>
                 )
             )}
